c9c6841ea1a529509319f2d08f4214e9223349f8
PabloVillapunMartin <pvillapu@ucm.es>
2020-05-07 16:16:37 +0200

    cummit

--- TrabajoDSI/TrabajoDSI/MainPage.xaml.cs
+++ TrabajoDSI/TrabajoDSI/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
@@ -25,6 +26,68 @@ namespace TrabajoDSI
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void FriendsSV_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            FriendsSV.IsPaneOpen = true;
+            SearchFriendsTextBox.Visibility = Visibility.Visible;
+        }
+
+        private void FriendsSV_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            FriendsSV.IsPaneOpen = false;
+            SearchFriendsTextBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void Image_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            CarImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/brilloCoche.png"));
+        }
+
+        private void Image_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            CarImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/lowPolyCar.png"));
+        }
+
+        private void Image_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            //Ir a garaje
+        }
+
+        private void thiefButton_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            thiefButton.Source = new BitmapImage(new Uri("ms-appx:///Assets/botonThiefPress.png"));
+        }
+
+        private void thiefButton_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            thiefButton.Source = new BitmapImage(new Uri("ms-appx:///Assets/botonThief.png"));
+        }
+
+        private void policeButton_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            policeButton.Source = new BitmapImage(new Uri("ms-appx:///Assets/PoliceButonPress.png"));
+        }
+
+        private void policeButton_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            policeButton.Source = new BitmapImage(new Uri("ms-appx:///Assets/PoliceButon.png"));
+        }
+
+        private void infoSV_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            infoSV.IsPaneOpen = true;
+        }
+
+        private void infoSV_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            infoSV.IsPaneOpen = false;
+        }
+
+        private void AjustesButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Ajustes));
         }
     }
 }
