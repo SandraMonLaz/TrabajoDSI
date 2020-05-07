94efdeb14e2b3843b1ab777bb7ea4c192287f0c0
Sandra <samondra@ucm.es>
2020-05-07 14:47:29 +0200

    Cambios

--- NonStopDriver/Garaje.xaml.cs
+++ NonStopDriver/Garaje.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +26,138 @@ namespace NonStopDriver
         public Garaje()
         {
             this.InitializeComponent();
+        }
+
+
+        private void StackMotor_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BtonSelec.png"));
+            StackMotor.Background = a;
+        }
+
+        private void StackMotor_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/Boton.png"));
+            StackMotor.Background = a;
+        }
+
+        private void StackMotor_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BotonPRS.png"));
+            StackMotor.Background = a;
+
+            feedbackruedas.Visibility = Visibility.Collapsed;
+            feedbackpara.Visibility = Visibility.Collapsed;
+            feedbackcolor.Visibility = Visibility.Collapsed;
+            motor.Visibility = Visibility.Visible;
+        }
+        private void StackMotor_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BtonSelec.png"));
+            StackMotor.Background = a;
+        }
+
+        private void StackRuedas_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BtonSelec.png"));
+            StackRuedas.Background = a;
+        }
+
+        private void StackRuedas_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/Boton.png"));
+            StackRuedas.Background = a;
+        }
+
+        private void StackRuedas_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BotonPRS.png"));
+            StackRuedas.Background = a;
+
+            feedbackruedas.Visibility = Visibility.Visible;
+            feedbackpara.Visibility = Visibility.Collapsed;
+            feedbackcolor.Visibility = Visibility.Collapsed;
+            motor.Visibility = Visibility.Collapsed;
+        }
+
+        private void StackRuedas_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BtonSelec.png"));
+            StackRuedas.Background = a;
+        }
+
+        private void StackParachoques_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BtonSelec.png"));
+            StackParachoques.Background = a;
+        }
+
+        private void StackParachoques_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/Boton.png"));
+            StackParachoques.Background = a;
+        }
+
+        private void StackParachoques_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BotonPRS.png"));
+            StackParachoques.Background = a;
+
+            feedbackruedas.Visibility = Visibility.Collapsed;
+            feedbackpara.Visibility = Visibility.Visible;
+            feedbackcolor.Visibility = Visibility.Collapsed;
+            motor.Visibility = Visibility.Collapsed;
+        }
+
+        private void StackParachoques_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BtonSelec.png"));
+            StackParachoques.Background = a;
+        }
+
+        private void StackColor_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BtonSelec.png"));
+            StackColor.Background = a;
+        }
+
+        private void StackColor_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/Boton.png"));
+            StackColor.Background = a;
+        }
+
+        private void StackColor_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BotonPRS.png"));
+            StackColor.Background = a;
+
+            feedbackruedas.Visibility = Visibility.Collapsed;
+            feedbackpara.Visibility = Visibility.Collapsed;
+            feedbackcolor.Visibility = Visibility.Visible;
+            motor.Visibility = Visibility.Collapsed;
+        }
+
+        private void StackColor_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("ms-appx:///" + "Assets/Garaje/BtonSelec.png"));
+            StackColor.Background = a;
         }
     }
 }
